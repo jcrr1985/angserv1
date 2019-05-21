f92ea68875bbfeff6c896fe2ac28b935bf7ea25c
Julio Cesar <julio.rodriguezr1@gmail.com>
2019-05-21 18:35:00 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  UserService} from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angserv1';
+export class AppComponent implements OnInit {
+
+  users = [];
+
+  datafromService;
+
+  constructor(protected userService: UserService) {}
+
+  ngOnInit() {
+
+   this.userService.getUsers()
+      .subscribe(
+        (data) => {
+          for (let index = 0; index < 5; index++) {
+          this.users.push(data['results'][index]);
+
+          }
+
+          console.log( this.users);
+        },
+        (error) => {
+          console.error(error);
+      }
+    );
+
+  }
+
 }
